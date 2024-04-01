17abbc70c20a0296e457c6f3b17ec52f4e687790
santiagohch <80603960+shchUNAL@users.noreply.github.com>
2024-03-31 22:48:13 -0500

    AG "Hecho"

--- API_Gateway/resolvers.py
+++ API_Gateway/resolvers.py
@@ -1,49 +1,69 @@
 import requests
-
-##Se definen url's principales para cada microservicio:
-url_users='http://localhost:3001/parchate/user/'
-url_vaca='http://localhost:8080/parchate/vaca/'
-
-
-
-##resolvers para el microservicio de usuarios
-def register_user(info, email, username, password):
-    response = requests.post(url_users+'register/', json={'email': email,'username': username, 'password': password})
-    return response.json()
-
-def login_user(info, email, password):
-    response = requests.post(url_users+'login/', json={'email': email, 'password': password})
-    return response.json()
-
-def get_user_profile(info):
-    response = requests.get(url_users+'profile', headers={'Authorization': info.context.headers.get('Authorization')})
-    return response.json()
-
-def logout_user(info):
-    response = requests.post(url_users+'logout', json={},headers={'Authorization': info.context.headers.get('Authorization')})
-    return response.json()
+import os
+# ##Se definen url's principales para cada microservicio:
+# url_users='http://localhost:3001/parchate/user/'
+# url_vaca='http://localhost:8080/parchate/vaca/'
+url_comentarios=os.environ.get('COMENTARIOS_MS_URL','host.docker.internal:3000/comentarios')
 
 
-##resolvers para el microservicio vaca
-def create_vaca(info,idPlan,nombreVaca,fechaLimite,montoTotal,Alcance):
-    response = requests.post(url_vaca+'crear/', json={'idPlan': idPlan,'nombreVaca': nombreVaca, 'fechaLimite': fechaLimite,'montoTotal': montoTotal,'Alcance':Alcance})
-    return response.json()
 
-def get_vaca(info,id_vaca):
-    response = requests.get(url_vaca+'buscar/'+str(id_vaca))
-    return response.json()
+# ##resolvers para el microservicio de usuarios
+# def register_user(info, email, username, password):
+#     response = requests.post(url_users+'register/', json={'email': email,'username': username, 'password': password})
+#     return response.json()
 
-def abonar_vaca(info,id_vaca,montototal):
-    response = request.put(url_vaca+'abonar', json={'id_vaca':id_vaca,'montototal':montototal})
-    return response.json()
+# def login_user(info, email, password):
+#     response = requests.post(url_users+'login/', json={'email': email, 'password': password})
+#     return response.json()
 
-def eliminar_vaca(info,id_vaca):
-    response = requests.delete(url_vaca+'eliminar/'+str(id_vaca))
-    return response.json()
+# def get_user_profile(info):
+#     response = requests.get(url_users+'profile', headers={'Authorization': info.context.headers.get('Authorization')})
+#     return response.json()
+
+# def logout_user(info):
+#     response = requests.post(url_users+'logout', json={},headers={'Authorization': info.context.headers.get('Authorization')})
+#     return response.json()
+
+
+# ##resolvers para el microservicio vaca
+# def create_vaca(info,idPlan,nombreVaca,fechaLimite,montoTotal,Alcance):
+#     response = requests.post(url_vaca+'crear/', json={'idPlan': idPlan,'nombreVaca': nombreVaca, 'fechaLimite': fechaLimite,'montoTotal': montoTotal,'Alcance':Alcance})
+#     return response.json()
+
+# def get_vaca(info,id_vaca):
+#     response = requests.get(url_vaca+'buscar/'+str(id_vaca))
+#     return response.json()
+
+# def abonar_vaca(info,id_vaca,montototal):
+#     response = request.put(url_vaca+'abonar', json={'id_vaca':id_vaca,'montototal':montototal})
+#     return response.json()
+
+# def eliminar_vaca(info,id_vaca):
+#     response = requests.delete(url_vaca+'eliminar/'+str(id_vaca))
+#     return response.json()
 
 
 ##resolvers para el microservicio comentarios
 
+def create_comentario(info, id_plan, nickname, comentario, rating):
+    response = requests.post(url_comentarios, json={'id_plan': id_plan, 'nickname': nickname, 'comentario': comentario,'rating': rating})
+    return response.json()
+
+def get_comentario(info, id_comentario):
+    response = requests.get(url_comentarios + str(id_comentario))
+    return response.json()
+
+def editar_comentario(info, id_comentario, comentario):
+    response = request.put(url_comentarios , json={'id_comentario':id_comentario,'comentario':comentario})
+    return response.json()
+
+def editar_rating(info, id_comentario, rating):
+    response = request.put(url_comentarios , json={'id_comentario':id_comentario,'rating':rating})
+    return response.json()    
+
+def eliminar_comentario(info, id_comentario):
+    response = requests.delete(url_comentarios + str(id_comentario))
+    return response.json()
 
 ##resolvers para el microservicio planes
 
--- API_Gateway/schema.py
+++ API_Gateway/schema.py
@@ -2,103 +2,151 @@
 import graphene
 from graphene import ObjectType, Schema
 from graphene.types.datetime import DateTime
-from resolvers import register_user, login_user, get_user_profile, logout_user, create_vaca, get_vaca, abonar_vaca, eliminar_vaca
+# from resolvers import register_user, login_user, get_user_profile, logout_user, create_vaca, get_vaca, abonar_vaca, eliminar_vaca
+from resolvers import create_comentario, get_comentario, editar_comentario, editar_rating, eliminar_comentario
 
+# ##Agrego Mutaciones para microservicio Usuarios
+# class RegisterMutation(graphene.Mutation):
+#     class Arguments:
+#         email = graphene.String(required=True)
+#         username = graphene.String(required=True)
+#         password = graphene.String(required=True)
 
-##Agrego Mutaciones para microservicio Usuarios
-class RegisterMutation(graphene.Mutation):
-    class Arguments:
-        email = graphene.String(required=True)
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
+#     response = graphene.String()
 
-    response = graphene.String()
+#     def mutate(self, info, email, username, password):
+#         response = register_user(info, email, username, password)
+#         return RegisterMutation(response=response)
 
-    def mutate(self, info, email, username, password):
-        response = register_user(info, email, username, password)
-        return RegisterMutation(response=response)
+# class LoginMutation(graphene.Mutation):
+#     class Arguments:
+#         email = graphene.String(required=True)
+#         password = graphene.String(required=True)
 
-class LoginMutation(graphene.Mutation):
-    class Arguments:
-        email = graphene.String(required=True)
-        password = graphene.String(required=True)
-
-    response = graphene.String()
+#     response = graphene.String()
     
-    def mutate(self, info, email, password):
-        response = login_user(info, email, password)
-        return LoginMutation(response=response)
+#     def mutate(self, info, email, password):
+#         response = login_user(info, email, password)
+#         return LoginMutation(response=response)
 
-class LogoutMutation(graphene.Mutation):
+# class LogoutMutation(graphene.Mutation):
+#     class Arguments:
+#         None
+#     response = graphene.String() 
+#     def mutate(self, info):
+#         response = logout_user(info)
+#         return LogoutMutation(response=response)
+
+# ##Agrego Mutaciones para microservicio vaca
+
+# class CreateVacaMutation(graphene.Mutation):
+#     class Arguments:
+#         idPlan = graphene.Int(required=True)
+#         nombreVaca = graphene.String(required=True)
+#         fechaLimite = DateTime(required=False)
+#         montoTotal = graphene.Float(required=True)
+#         Alcance = graphene.String(required=True)
+
+#     response = graphene.String()
+#     def mutate(self, info, idPlan, nombreVaca, fechaLimite, montoTotal, Alcance):
+#         response = create_vaca(info, idPlan, nombreVaca, fechaLimite, montoTotal, Alcance)
+#         return CreateVacaMutation(response=response)
+
+# class UpdateVacaMutation(graphene.Mutation):
+#     class Arguments:
+#         id_vaca = graphene.Int(required=True)
+#         montototal = graphene.Float(required=True)
+
+#     response = graphene.String()
+
+#     def mutate(self, info, id_vaca, montototal):
+#         response = abonar_vaca(info, id_vaca, montototal)
+#         return UpdateVacaMutation(response=response)
+
+# class EliminarVacaMutation(graphene.Mutation):
+#     class Arguments:
+#         id_vaca = graphene.Int(required=True)
+#     response = graphene.String()
+
+#     def mutate(self, info, id_vaca):
+#         response = eliminar_vaca(info, id_vaca)
+#         return EliminarVacaMutation(response=response)
+
+# ##Agrego Mutaciones para microservicio comentarios
+
+class CreateComentarioMutation(graphene.Mutation):
     class Arguments:
-        None
-    response = graphene.String() 
-    def mutate(self, info):
-        response = logout_user(info)
-        return LogoutMutation(response=response)
-
-##Agrego Mutaciones para microservicio vaca
-
-class CreateVacaMutation(graphene.Mutation):
-    class Arguments:
-        idPlan = graphene.Int(required=True)
-        nombreVaca = graphene.String(required=True)
-        fechaLimite = DateTime(required=False)
-        montoTotal = graphene.Float(required=True)
-        Alcance = graphene.String(required=True)
+        id_plan = graphene.Int(required=True)
+        nickname = graphene.String(required=True)
+        comentario = graphene.String(required=True)
+        rating = graphene.Int(required=True)
 
     response = graphene.String()
-    def mutate(self, info, idPlan, nombreVaca, fechaLimite, montoTotal, Alcance):
-        response = create_vaca(info, idPlan, nombreVaca, fechaLimite, montoTotal, Alcance)
-        return CreateVacaMutation(response=response)
+    def mutate(self, info, id_plan, nickname, comentario, rating):
+        response = create_comentario(info, id_plan, nickname, comentario, rating)
+        return CreateComentarioMutation(response=response)
 
-class UpdateVacaMutation(graphene.Mutation):
+class UpdateComentarioMutation(graphene.Mutation):
     class Arguments:
-        id_vaca = graphene.Int(required=True)
-        montototal = graphene.Float(required=True)
+        id_comentario = graphene.String(required=True)
+        comentario = graphene.String(required=True)
 
     response = graphene.String()
+    def mutate(self, info, id_comentario, comentario):
+        response = editar_comentario(info, id_comentario, comentario)
+        return UpdateComentarioMutation(response=response)
 
-    def mutate(self, info, id_vaca, montototal):
-        response = abonar_vaca(info, id_vaca, montototal)
-        return UpdateVacaMutation(response=response)
-
-class EliminarVacaMutation(graphene.Mutation):
+class UpdateRatingMutation(graphene.Mutation):
     class Arguments:
-        id_vaca = graphene.Int(required=True)
+        id_comentario = graphene.String(required=True)
+        rating = graphene.Int(required=True)
+
     response = graphene.String()
+    def mutate(self, info, id_comentario, rating):
+        response = editar_rating(info, id_comentario, rating)
+        return UpdateRatingMutation(response=response)
 
-    def mutate(self, info, id_vaca):
-        response = eliminar_vaca(info, id_vaca)
-        return EliminarVacaMutation(response=response)
+class EliminarComentarioMutation(graphene.Mutation):
+    class Arguments:
+        id_comentario = graphene.String(required=True)
 
-##Agrego Mutaciones para microservicio comentarios
+    response = graphene.String()
+    def mutate(self, info, id_comentario):
+        response = eliminar_comentario(info, id_vaca)
+        return EliminarComentarioMutation(response=response)
+
+# ##Agrego Mutaciones para microservicio planes
 
 
-
-##Agrego Mutaciones para microservicio planes
-
-
-##Se agregan todas las mutaciones
+# ##Se agregan todas las mutaciones
 class Mutation(graphene.ObjectType):
-    register_user = RegisterMutation.Field()
-    login_user = LoginMutation.Field()
-    logout_user = LogoutMutation.Field()
-    create_vaca = CreateVacaMutation.Field()
-    update_vaca = UpdateVacaMutation.Field()
-    eliminar_vaca = EliminarVacaMutation.Field()
+#     register_user = RegisterMutation.Field()
+#     login_user = LoginMutation.Field()
+#     logout_user = LogoutMutation.Field()
+#     create_vaca = CreateVacaMutation.Field()
+#     update_vaca = UpdateVacaMutation.Field()
+#     eliminar_vaca = EliminarVacaMutation.Field()
+    create_comentario = CreateComentarioMutation.Field()
+    update_comentario = UpdateComentarioMutation.Field()
+    update_rating = UpdateRatingMutation.Field()
+    delete_comentario = EliminarComentarioMutation.Field()
 
-##Se crean las querys (Solicitudes get)
+# ##Se crean las querys (Solicitudes get)
 class Query(ObjectType):
-    user_profile = graphene.String()
-    vaca_info = graphene.String(id_vaca=graphene.Int(required=True))
+    comentario_info = graphene.String(id_comentario = graphene.String(required=True))
+#     user_profile = graphene.String()
+#     vaca_info = graphene.String(id_vaca=graphene.Int(required=True))
 
-    def resolve_user_profile(self, info):
-        response = get_user_profile(info)
-        return response
+#     def resolve_user_profile(self, info):
+#         response = get_user_profile(info)
+#         return response
 
-    def resolve_vaca_info(self, info, id_vaca):
-        response = get_vaca(info, id_vaca)
+#     def resolve_vaca_info(self, info, id_vaca):
+#         response = get_vaca(info, id_vaca)
+#         return response
+
+    def resolve_comentario_info(self, id_comentario):
+        response = get_comentario(info, id_comentario)
         return response
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
