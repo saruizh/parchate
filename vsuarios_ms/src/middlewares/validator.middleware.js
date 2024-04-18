9d1913ca3c155b7c5186eb36fcfd2abadb47fddd
jortegar18 <72839162+jortegar18@users.noreply.github.com>
2024-04-17 22:36:33 -0500

    usuarios-ms updated

--- vsuarios_ms/src/middlewares/validator.middleware.js
+++ vsuarios_ms/src/middlewares/validator.middleware.js
@@ -1,9 +1,43 @@
-export const validateSchema = (schema) => (req, res, next) =>{
-    try {
-        schema.parse(req.body);
-        next();
-    } catch (error) {
-        return res.status(400).json({error:error.errors.map(error=>error.message)});
-        
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model.js');
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+  
+    // check json web token exists & is verified
+    if (token) {
+      jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
+        if (err) {
+          console.log(err.message);
+          res.redirect('/login');
+        } else {
+          console.log(decodedToken);
+          next();
+        }
+      });
+    } else {
+      res.redirect('/login');
     }
-};
+  };
+  
+  // check current user
+  const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+      jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+        if (err) {
+          res.locals.user = null;
+          next();
+        } else {
+          let user = await User.findById(decodedToken.id);
+          res.locals.user = user;
+          next();
+        }
+      });
+    } else {
+      res.locals.user = null;
+      next();
+    }
+  };
+  
+  
+  module.exports = { requireAuth, checkUser };
