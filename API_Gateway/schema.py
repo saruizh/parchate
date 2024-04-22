ea5efa2cdf4180d671ce001b6d80b252283edad2
santiagohch <80603960+shchUNAL@users.noreply.github.com>
2024-04-21 21:14:44 -0500

    cambios varios

--- API_Gateway/schema.py
+++ API_Gateway/schema.py
@@ -9,27 +9,28 @@ from resolvers import register_user, login_user, get_user_profile, logout_user, 
 ##Agrego Mutaciones para microservicio Usuarios
 class RegisterMutation(graphene.Mutation):
     class Arguments:
-        idPlan = graphene.Int(required=True)
-        nickname = graphene.String(required=True)
-        cuerpo = graphene.String(required=True)
-        rating = graphene.Int(required=True)
+        email = graphene.String(required=True)
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
 
     response = graphene.String()
-    def mutate(self, info, idPlan, nickname, cuerpo, rating):
-        response = create_comentario(info, idPlan, nickname, cuerpo, rating)
-        return CreateComentarioMutation(response=response)
 
-class UpdateComentarioMutation(graphene.Mutation):
+    def mutate(self, info, email, username, password):
+        response = register_user(info, email, username, password)
+        return RegisterMutation(response=response)
+
+class LoginMutation(graphene.Mutation):
     class Arguments:
-        idComentario = graphene.String(required=True)
-        cuerpo = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
 
     response = graphene.String()
-    def mutate(self, info, idComentario, cuerpo):
-        response = editar_comentario(info, idComentario, cuerpo)
-        return UpdateComentarioMutation(response=response)
+    
+    def mutate(self, info, email, password):
+        response = login_user(info, email, password)
+        return LoginMutation(response=response)
 
-class UpdateRatingMutation(graphene.Mutation):
+class LogoutMutation(graphene.Mutation):
     class Arguments:
         None
     response = graphene.String() 
